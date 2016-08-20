57892311146264b374c2f2cb82e38bbbf663a45e
Richard Warrington <richardjwarrington@gmail.com>
2016-08-20 08:10:13 -0700

    Initial Commit

---
+++ Swift3 App 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift3 App 1
+//
+//  Created by Macbook Pro on 2016-08-20.
+//  Copyright © 2016 SpeedBird. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
